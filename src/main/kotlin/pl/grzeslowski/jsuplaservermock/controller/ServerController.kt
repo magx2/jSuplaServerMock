4d1e01440cfda59683f156b7adb01a19503313c9
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-05-20 19:07:31 +0200

    #9 Remove get mapping

--- src/main/kotlin/pl/grzeslowski/jsuplaservermock/controller/ServerController.kt
+++ src/main/kotlin/pl/grzeslowski/jsuplaservermock/controller/ServerController.kt
@@ -4,15 +4,12 @@ import io.swagger.api.ServerInfoApi
 import io.swagger.api.ServerStatusApi
 import io.swagger.model.ServerInfo
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import pl.grzeslowski.jsuplaservermock.Database
 
 @RestController
 class ServerController(val database: Database) : ServerInfoApi, ServerStatusApi {
-    @GetMapping("/server-info")
     override fun getServerInfo(): ResponseEntity<ServerInfo> = ResponseEntity.ok(database.serverInfo)
 
-    @GetMapping("/server-status")
     override fun getSuplaServerStatus(): ResponseEntity<Void> = ResponseEntity.ok().build()
 }
