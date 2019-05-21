0794090a8de0e0baba9ac6505605e92c96cf42c3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-05-21 22:19:55 +0200

    #7 Remove Database interface to use services per model

--- src/main/kotlin/pl/grzeslowski/jsuplaservermock/InMemoryDatabase.kt
+++ src/main/kotlin/pl/grzeslowski/jsuplaservermock/InMemoryDatabase.kt
@@ -1,43 +1,14 @@
 package pl.grzeslowski.jsuplaservermock
 
-import io.swagger.model.Channel
-import io.swagger.model.Device
 import io.swagger.model.ServerInfo
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.*
-import java.util.stream.Collectors
+import pl.grzeslowski.jsuplaservermock.service.ServerService
 
 @Service
 class InMemoryDatabase(
         @Value("\${server.servlet.context-path}") val contextPath: String,
-        @Value("\${server.port}") val port: String) : Database {
-    private val devices = HashSet<Device>()
-
-    override fun findAllDevices(): List<Device> = ArrayList(devices)
-
-
-    override fun findDevice(id: Int): Optional<Device> =
-            devices.stream()
-                    .filter { d -> d.id == id }
-                    .findAny()
-
-    override fun addDevice(device: Device) {
-        devices.add(device)
-    }
-
-    override fun findAllChannels(): List<Channel> =
-            devices.stream()
-                    .map { it.channels }
-                    .flatMap { it.stream() }
-                    .collect(Collectors.toList())
-
-    override fun findChannel(id: Int): Optional<Channel> =
-            findAllChannels()
-                    .stream()
-                    .filter { c -> c.id == id }
-                    .findAny()
-
+        @Value("\${server.port}") val port: String) : ServerService {
     override fun getServerInfo(): ServerInfo =
             ServerInfo()
                     .apiVersion("2.3.0")
