e903fea2be88935d6581e0fd0559dd17f552047a
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-08-04 20:20:08 +0200

    #38 Use base packages for `pl.grzeslowski.jsuplaservermock`

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -9,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = {"io.swagger", "io.swagger.api", "io.swagger.configuration"}, basePackageClasses = Swagger2SpringBoot.class)
+@ComponentScan(basePackages = {"io.swagger", "io.swagger.api", "io.swagger.configuration", "pl.grzeslowski.jsuplaservermock"})
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
