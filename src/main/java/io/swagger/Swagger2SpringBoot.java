648c2e48add5f3dc3c72b539fe12b8ba51863a60
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-08-04 20:13:56 +0200

    #38 Fix code after removing  `DatabaseInitializer`

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -5,12 +5,11 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import pl.grzeslowski.jsuplaservermock.DatabaseInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = {"io.swagger", "io.swagger.api", "io.swagger.configuration"}, basePackageClasses = DatabaseInitializer.class)
+@ComponentScan(basePackages = {"io.swagger", "io.swagger.api", "io.swagger.configuration"}, basePackageClasses = Swagger2SpringBoot.class)
 public class Swagger2SpringBoot implements CommandLineRunner {
 
     @Override
